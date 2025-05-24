3ff186adcbb27787d118a2a5b66424f07913e1f3
a41s <amr41sal@hotmail.com>
2025-05-24 15:13:18 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Description: Print all single digir numbers of base 10 starting from 0.
+ * Return: 0 (Success).
+ */
+int main(void)
+{
+	short i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
