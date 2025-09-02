cb5a5f583d250822865d8555ac39b97f7b3968ee
a41s <amr41sal@hotmail.com>
2025-09-02 09:13:26 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,16 @@ char *_strncat(char *dest, char *src, int n)
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	while (dest[i])
 		i++;
 
 	j = 0;
-	while (j < n)
+	while (j < n && src[j] != '\0')
 	{
 		dest[i++] += src[j];
 		j++;
 	}
 
+	dest += '\0';
 	return (dest);
 }
