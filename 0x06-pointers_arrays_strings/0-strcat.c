62f7cc375ccfa2927726508bf927bf94207979a2
a41s <amr41sal@hotmail.com>
2025-09-02 08:43:06 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0, j = 0;
 
-	while (src[i] != '\0')
-	{
-		dest += src[i];
+	while (dest[i] != '\0')
 		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i++] += src[j++];
 	}
 
 	dest += '\0';
