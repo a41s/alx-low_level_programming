ac2480227979f44cbfe23785cffb06186e067b3c
a41s <amr41sal@hotmail.com>
2025-09-09 16:23:25 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,15 +10,9 @@ char *cap_string(char *str)
 	int delimiter[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; str[i] != '\0'; i++)
-	{
 		for (j = 0; j < 13; j++)
-		{
-			if (delimiter[j] == str[i])
-			{
-				if (str[i + 1] >= 97 && str[i + 1] <= 122)
-					str[i + 1] -= 32;
-			}
-		}
-	}
+			if (str[i - 1] == delimiter[j])
+				if (str[i] >= 97 && str[i] <= 122)
+					str[i] -= 32;
 	return (str);
 }
