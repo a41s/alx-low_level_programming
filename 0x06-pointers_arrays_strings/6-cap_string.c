a7a393a41832bbb24de24b8e3b233a93423b4964
a41s <amr41sal@hotmail.com>
2025-09-09 17:32:46 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,30 @@
 #include "main.h"
 /**
  * cap_string - A function that capitalizes all words of a string.
- * @str: An input.
+ * @s: An input.
  * Return: A string.
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
-	int i, j;
-	int delimiter[13] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
-		for (j = 0; j < 13; j++)
-			if (str[i - 1] == delimiter[j])
-				if (str[i] >= 97 && str[i] <= 122)
-					str[i] -= 32;
-	return (str);
+	if (s[0] >= 97 && s[0] <= 122)
+		s[0] = s[0] - 32;
+
+	while (s[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+			|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+			|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+			|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+			|| s[count] == '{' || s[count] == '}')
+		{
+			if (s[count + 1] >= 97 && s[count + 1] <= 122)
+			{
+				s[count + 1] = s[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (s);
 }
