9bedb81048ed62db413135ab7cb47120f974e9a2
a41s <amr41sal@hotmail.com>
2025-10-18 11:01:39 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,11 @@ int _isalpha(int);
 int _islower(int);
 void print_alphabet_x10(void);
 void print_alphabet(void);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 int _putchar(char);
 
 #endif
